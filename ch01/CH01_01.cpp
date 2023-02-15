805e94ee5e282391e0dbbc7e339d70c2bf49a741
王彥儒 <125424141+Yan-Ju-Wang@users.noreply.github.com>
2023-02-15 15:17:16 +0800

    Add files via upload

---
+++ ch01/CH01_01.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    cout<<"我的第一個C++程式"<<endl;
+    //列印字串 
+    system("pause");
+    return 0;
+}  
---
+++ ch01/ex01_02.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cstdlib>
+
+int main()
+{
+    std::cout<<"我的第一個C++程式"<<std::endl;//列印字串 
+    
+    system("pause");
+    return 0;
+}  
