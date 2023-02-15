0e69532e0bf3ac0e629215c33e1355df5598d296
王彥儒 <125424141+Yan-Ju-Wang@users.noreply.github.com>
2023-02-15 15:18:17 +0800

    Add files via upload

---
+++ ch02/ex02_02.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    char ch='A'; //直接指定'A'字元 
+    cout<<"ch= "<<ch<<endl;
+    ch=65; //10位數值表示 A字元 
+    cout<<"ch= "<<ch<<endl;
+    ch='\x41';   //16進位ASCII碼表示 A字元 
+    cout<<"ch= "<<ch<<endl;
+    ch=0x41;    //16進位數值表示 A字元 
+    cout<<"ch= "<<ch<<endl;
+    ch='\101';   //8進位ASCII碼表示 A字元 
+    cout<<"ch= "<<ch<<endl;
+    ch=0101;    //8進位數值表示 A字元 	  
+    cout<<"ch= "<<ch<<endl;
+    
+    system("pause");
+    return 0;
+}
+		 
+		 
---
+++ ch02/ex02_06.cpp
@@ -0,0 +1,17 @@
+ //顯示變數資料型態的長度
+#include<iostream>
+#include<cstdlib>
+
+using namespace std;
+
+int main()
+{
+    int salary=100;//宣告為整數型態 
+    float sum=100.99; //宣告為實數型態 
+    
+    cout<<"salary的資料長度＝"<<sizeof(salary)<<"位元組"<<endl;  //取得變數salary大小
+    cout<<"sum的資料長度＝"<<sizeof(sum)<<"位元組"<<endl;  //取得變數sum大小
+    
+    system("pause");
+    return 0;
+}
